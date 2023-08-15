3fa1e570270d038a601e08bc9c4af4e3db2d3f0d
danxiaonuo <danxiaonuo@danxiaonuo.me>
2023-08-15 16:56:30 +0800

    Create myscript.sh

---
+++ myscript.sh
@@ -0,0 +1,5 @@
+# python /app/chatgpt/autorestart.py
+rm -rf /app/pandora/token.txt
+wget --no-check-certificate https://down.xiaonuo.live/?url=https://raw.githubusercontent.com/danxiaonuo/gwf/main/chatgpt/chatgpt_token.txt -O /app/pandora/token.txt
+cd /app/pandora
+nohup pandora -s 0.0.0.0:8008 -t token.txt > output.log 2>&1 &
