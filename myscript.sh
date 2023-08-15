83d36f47281967fe20925f0169cefd8a3237eec1
danxiaonuo <danxiaonuo@danxiaonuo.me>
2023-08-15 17:26:40 +0800

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
+# chatgpt token
+TOKEN_TMP=$(wget -q -O - https://down.xiaonuo.live/?url=https://raw.githubusercontent.com/danxiaonuo/gwf/main/chatgpt/chatgpt_token.txt)
+ACCESS_TOKEN="${ACCESS_TOKEN:=${TOKEN_TMP}}"
+
 # python /app/chatgpt/autorestart.py
+
 kill -9 $(lsof -t -i:8008)
-rm -rf /app/pandora/token.txt
-wget --no-check-certificate https://down.xiaonuo.live/?url=https://raw.githubusercontent.com/danxiaonuo/gwf/main/chatgpt/chatgpt_token.txt -O /app/pandora/token.txt
+
+# 运行 pandora
+echo "${ACCESS_TOKEN}" > /app/pandora/token.txt
 cd /app/pandora
 nohup pandora -s 0.0.0.0:8008 -t token.txt > output.log 2>&1 &
