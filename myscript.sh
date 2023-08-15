7144d7cb46fe13a1b39c2a0a3665d5ecc84ab51d
danxiaonuo <danxiaonuo@danxiaonuo.me>
2023-08-15 17:06:17 +0800

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,4 +1,5 @@
-python /app/chatgpt/autorestart.py
+# python /app/chatgpt/autorestart.py
+kill -9 $(lsof -t -i:8008)
 rm -rf /app/pandora/token.txt
 wget --no-check-certificate https://down.xiaonuo.live/?url=https://raw.githubusercontent.com/danxiaonuo/gwf/main/chatgpt/chatgpt_token.txt -O /app/pandora/token.txt
 cd /app/pandora
