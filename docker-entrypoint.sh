50ea42361996cf2d80ccbc55e32348ebc0b71e7b
danxiaonuo <danxiaonuo@danxiaonuo.me>
2023-10-10 12:55:41 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# chatgpt token
-# TOKEN_TMP=$(wget -q -O - https://down.xiaonuo.live/?url=https://raw.githubusercontent.com/danxiaonuo/gwf/main/chatgpt/chatgpt_token.txt)
-# ACCESS_TOKEN="${ACCESS_TOKEN:=${TOKEN_TMP}}"
+# 输入账号信息
 echo "${account}" > /app/chatgpt/user.txt
+
+# 获取chatgpt token
+# TOKEN_TMP=$(wget -q -O - https://down.xiaonuo.live/?url=https://raw.githubusercontent.com/danxiaonuo/gwf/main/chatgpt/chatgpt_token.txt)
+bash myscript.sh
+TOKEN_TMP=$(cat /app/chatgpt/chatgpt_token.txt)
+ACCESS_TOKEN="${ACCESS_TOKEN:=${TOKEN_TMP}}"
 
 # 运行chatgpt web
 # echo "${ACCESS_TOKEN}" > /app/pandora/token.txt && cron && bash myscript.sh && cd /app/chatgpt && yarn start
-cron && bash myscript.sh && cd /app/chatgpt && yarn start
+echo "${ACCESS_TOKEN}" > /app/pandora/token.txt && cron && cd /app/chatgpt && yarn start
